3caa64bdac73857bfea3a01deadf09a461b34747
jhonSMH <jhon.mafla@correounivalle.edu.co>
2025-05-09 00:44:04 -0500

    22

--- servidor/index.js
+++ servidor/index.js
@@ -7,6 +7,16 @@ const router = require('./routes');
 
 
 const app = express();
+// redireccion de www a sin www
+app.use((req, res, next) => {
+    if (req.headers.host.startsWith('www.')) {
+        const newHost = req.headers.host.replace('www.', '');
+        return res.redirect(301, `${req.protocol}://${newHost}${req.originalUrl}`);
+    }
+    next();
+});
+
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
